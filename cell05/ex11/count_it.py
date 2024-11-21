43af6d3836464969d9f272ac02c1f65887f1b7da
saranyapat3368 <saranyapat3368@gmail.com>
2024-11-21 11:51:04 +0000

    5.11

---
+++ cell05/ex11/count_it.py
@@ -0,0 +1,11 @@
+import sys
+
+if len(sys.argv) > 1:
+    
+    print(f"parameters: {len(sys.argv) - 1}")
+    
+    for param in sys.argv[1:]:
+        print(f"{param}: {len(param)}")
+else:
+    
+    print("none")
